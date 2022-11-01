8cd252129d343b5443de904fbbfccf4bfdb0b9a7
Tricky Licky <wangombevictor5@gmail.com>
2022-11-01 12:09:13 +0300

    complete assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
-
+require_relative './user'
+class Student < User 
+    def initialize
+        @knowledge = []
+    end
+    def learn(words)
+        @knowledge << words
+    end
+    def knowledge
+        @knowledge
+    end
 end
